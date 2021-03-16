86e24bf39495962e1d2372de16d7776871772266
NicolasVDiSi <80472395+NicolasVDiSi@users.noreply.github.com>
2021-03-16 10:05:15 -0300

    Cambios Mobile
    Codigo para que no se vean los botones de grid y row en mobile

--- Sacha Shoes_archivos/Javascript/sachaCatalog.js
+++ Sacha Shoes_archivos/Javascript/sachaCatalog.js
@@ -1,14 +1,14 @@
 //cambiar vista del catalogo
 function mobile() {
     w_w = window.innerWidth;
-    if (w_w < 510) {
+  if (w_w < 510) {
         document.getElementById("cardProducto").classList.replace('ui-search-layout--stack', 'ui-search-layout--grid');
-        document.getElementById("active").classList.remove("ui-search-view-change__link--active");
-        document.getElementById("dactive").classList.add("ui-search-view-change__link--active");
+        document.getElementById("active").style.display = "none";
+        document.getElementById("dactive").style.display = "none";
     }else if (w_w > 510) {
         document.getElementById("cardProducto").classList.replace('ui-search-layout--grid', 'ui-search-layout--stack');
-        document.getElementById("dactive").classList.remove("ui-search-view-change__link--active");
-        document.getElementById("active").classList.add("ui-search-view-change__link--active");
+        document.getElementById("active").style.display = "flex";
+        document.getElementById("dactive").style.display = "flex";
     }
      
 }
