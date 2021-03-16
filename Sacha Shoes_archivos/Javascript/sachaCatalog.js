76ceb0b8d09283dc57eec1aa3106115e72967a3d
NicolasVDiSi <nicolas.di.si@infiniteloop.com.ar>
2021-03-16 10:08:26 -0300

    Update sachaCatalog.js

--- Sacha Shoes_archivos/Javascript/sachaCatalog.js
+++ Sacha Shoes_archivos/Javascript/sachaCatalog.js
@@ -3,12 +3,12 @@ function mobile() {
     w_w = window.innerWidth;
     if (w_w < 510) {
         document.getElementById("cardProducto").classList.replace('ui-search-layout--stack', 'ui-search-layout--grid');
-        document.getElementById("active").classList.remove("ui-search-view-change__link--active");
-        document.getElementById("dactive").classList.add("ui-search-view-change__link--active");
+        document.getElementById("active").style.display = "none";
+        document.getElementById("dactive").style.display = "none";
     }else if (w_w > 510) {
         document.getElementById("cardProducto").classList.replace('ui-search-layout--grid', 'ui-search-layout--stack');
-        document.getElementById("dactive").classList.remove("ui-search-view-change__link--active");
-        document.getElementById("active").classList.add("ui-search-view-change__link--active");
+        document.getElementById("active").style.display = "flex";
+        document.getElementById("dactive").style.display = "flex";
     }
      
 }
